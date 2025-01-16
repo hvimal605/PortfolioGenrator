bb24dbf072861e38f40316caf9d58101cd24d8a4
HARSH KUMAR VIMAL <140744904+hvimal605@users.noreply.github.com>
2025-01-17 00:31:09 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://127.0.0.1:5500",
         credentials: true,
     })
 )
